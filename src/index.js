febf6793287860f9446d19b60c0174db77dd5336
Beecallpaw <thapabeecallpaw@gmail.com>
2018-05-03 17:08:33 +0545

    Boilerplate Created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM, { render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(<h1>Hello</h1>, document.querySelector("#root"));
